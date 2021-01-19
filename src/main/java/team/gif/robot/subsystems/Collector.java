fc5e410fad702291a125f68766a9c30082edcf9f
Shrey Mathur <shreym01@outlook.com>
2021-01-18 20:16:41 -0600

    Initial Commit

---
+++ src/main/java/team/gif/robot/subsystems/Collector.java
@@ -0,0 +1,11 @@
+package team.gif.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import team.gif.robot.RobotMap;
+
+public class Collector {
+}
