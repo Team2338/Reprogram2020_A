7dd9b506b85ca063e956e5dc3626aa749afc1602
Rohan <gameracer007@gmail.com>
2021-01-18 21:07:40 -0600

    Basic shooter code
    Basic code for the shooter, still need work on PIDF coefficients

---
+++ src/main/java/team/gif/robot/commands/shooter/shooterCommand.java
@@ -0,0 +1,63 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package team.gif.robot.commands.shooter;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import team.gif.robot.Robot;
+import team.gif.robot.subsystems.ExampleSubsystem;
+import team.gif.robot.subsystems.Shooter;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class shooterCommand extends CommandBase {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
+
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public shooterCommand() {
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(Shooter.getInstance());
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        /**
+         * Shooter go pew pew
+         */
+        if (Robot.oi != null && Robot.oi.dLT.get()) {
+            Shooter.getInstance().setSpeed(10);
+        } else{
+            Shooter.getInstance().setSpeed(0);
+        }
+
+
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+}
---
+++ src/main/java/team/gif/robot/subsystems/Shooter.java
@@ -0,0 +1,40 @@
+package team.gif.robot.subsystems;
+
+import com.revrobotics.*;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import team.gif.robot.RobotMap;
+import edu.wpi.first.wpilibj.controller.PIDController;
+
+
+public class Shooter extends SubsystemBase {
+
+
+    private static Shooter instance = null;
+
+
+    public static Shooter getInstance() {
+
+        if (instance == null) {
+            instance = new Shooter();
+        }
+        return instance;
+    }
+
+    private static final CANSparkMax flywheelShooter = new CANSparkMax(RobotMap.FLYWHEEL, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private static final CANPIDController flywheelMotorPIDController = flywheelShooter.getPIDController();
+
+    private Shooter() {
+        //need to adjust to remain safe
+        flywheelShooter.restoreFactoryDefaults();
+        flywheelShooter.enableVoltageCompensation(11);
+
+        flywheelMotorPIDController.setP(0.0003);
+        flywheelMotorPIDController.setFF(0.00003);
+        flywheelMotorPIDController.setOutputRange(0,1);
+    }
+
+    public void setSpeed(double velocity) {
+        flywheelMotorPIDController.setReference(velocity, ControlType.kVelocity);
+
+    }
+}
